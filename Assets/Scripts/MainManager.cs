fdb00864e57d1a79f3da2335aea68250440ff9c6
Lythwain <46134682+Lythwain@users.noreply.github.com>
2021-11-06 09:20:13 -0400

    Final version
    Data for high score gets saved to file.  Also when blocks run out a new set of blocks are added to allow player to go on forever.

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using System.IO;
 
 public class MainManager : MonoBehaviour
 {
@@ -12,6 +13,7 @@ public class MainManager : MonoBehaviour
 
     public Text ScoreText;
     public Text TopScoreText;
+    public Text GameOverMessage;
     public GameObject GameOverText;
     
     private bool m_Started = false;
@@ -23,10 +25,14 @@ public class MainManager : MonoBehaviour
     void Start()
     {
         DisplayScore();
+        CreateBricks();
+    }
+    private void CreateBricks()
+    {
         const float step = 0.6f;
         int perLine = Mathf.FloorToInt(4.0f / step);
-        
-        int[] pointCountArray = new [] {1,1,2,2,5,5};
+
+        int[] pointCountArray = new[] { 1, 1, 2, 2, 5, 5 };
         for (int i = 0; i < LineCount; ++i)
         {
             for (int x = 0; x < perLine; ++x)
@@ -35,12 +41,16 @@ public class MainManager : MonoBehaviour
                 var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
                 brick.PointValue = pointCountArray[i];
                 brick.onDestroyed.AddListener(AddPoint);
+                DataManager.Instance.brickCount++;
             }
         }
     }
-
     private void Update()
     {
+        if(!m_GameOver && (DataManager.Instance.brickCount == 0))
+        {
+            CreateBricks();
+        }
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             SceneManager.LoadScene(0,LoadSceneMode.Single);
@@ -71,16 +81,51 @@ public class MainManager : MonoBehaviour
     void DisplayScore()
     {
         ScoreText.text = $"Player {DataManager.Instance.playerName} Score : {m_Points}";
+        GetHighScore();
     }
     void AddPoint(int point)
     {
         m_Points += point;
         DisplayScore();
     }
+    public int GetHighScore()
+    {
+        try
+        {
+            string path = Application.dataPath + "/HighScore.json";
+            string json = File.ReadAllText(path);
+            DataManager.Instance.data = JsonUtility.FromJson<SaveData>(json);
+            TopScoreText.text = $"Best Score : {DataManager.Instance.data.name} : {DataManager.Instance.data.score}";
+        }
+        catch(FileNotFoundException)
+        {
+            DataManager.Instance.data.score = m_Points;
+            DataManager.Instance.data.name = DataManager.Instance.playerName;
+            string json = JsonUtility.ToJson(DataManager.Instance.data);
+            File.WriteAllText(Application.dataPath + "/HighScore.json", json);
+        }
+        return DataManager.Instance.data.score;
+    }
 
+    public void SetHighScore()
+    {
+        int high = GetHighScore();
+        if(high > m_Points)
+        {
+            //this is not the high score so exit
+            return;
+        }
+        DataManager.Instance.data.score = m_Points;
+        DataManager.Instance.data.name = DataManager.Instance.playerName;
+        string json = JsonUtility.ToJson(DataManager.Instance.data);
+
+        File.WriteAllText(Application.dataPath + "/HighScore.json", json);
+    }
     public void GameOver()
     {
+        SetHighScore();
         m_GameOver = true;
+        GameOverMessage.text = "GAME OVER\nPress Space to Restart.\nHit Esc to return to main menu.";
         GameOverText.SetActive(true);
     }
 }
